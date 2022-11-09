01c887138b635761f512a51fddee62fcaf0bf2d6
mahmoud <eslamimahmoud98@gmail.com>
2022-11-09 04:23:33 +0330

    remove cubit files

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pomodore/core/constant/constant.dart';
 import 'package:pomodore/core/router/router.dart';
 import 'package:pomodore/core/utils/bloc_observer.dart';
-import 'package:pomodore/features/timer/presentation/bloc/timer/timer_cubit.dart';
 
 import 'core/utils/size_config.dart';
 
@@ -37,32 +36,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => TimerCubit()..start(),
-      child: OrientationBuilder(
-        builder: (context, orientation) =>
-            LayoutBuilder(builder: (context, constraints) {
-          SizeConfig().init(constraints, orientation);
-          return MaterialApp(
-            title: AppConstant.appName,
-            onGenerateRoute: AppRouter.onGenerationRouter,
-            theme: AppConstant.getTheme(context),
-            debugShowCheckedModeBanner: false,
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: const [
-              Locale('en'),
-              Locale('fa'),
-              Locale('de'),
-            ],
-            locale: const Locale('en'),
-          );
-        }),
-      ),
+    return OrientationBuilder(
+      builder: (context, orientation) =>
+          LayoutBuilder(builder: (context, constraints) {
+        SizeConfig().init(constraints, orientation);
+        return MaterialApp(
+          title: AppConstant.appName,
+          onGenerateRoute: AppRouter.onGenerationRouter,
+          theme: AppConstant.getTheme(context),
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('fa'),
+            Locale('de'),
+          ],
+          locale: const Locale('en'),
+        );
+      }),
     );
   }
 }
