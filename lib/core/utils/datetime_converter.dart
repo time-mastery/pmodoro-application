17c38370c8f0abbc97a0e79bfed322bcecbb86e3
mahmoud <eslamimahmoud98@gmail.com>
2022-11-09 03:38:17 +0330

    datetime_converter.dart class added

---
+++ lib/core/utils/datetime_converter.dart
@@ -0,0 +1,9 @@
+class DateTimeConverter {
+  static String formatSecToMinSec({required int timeInSecond}) {
+    int sec = timeInSecond % 60;
+    int min = (timeInSecond / 60).floor();
+    String minute = min.toString().length <= 1 ? "0$min" : "$min";
+    String second = sec.toString().length <= 1 ? "0$sec" : "$sec";
+    return "$minute : $second";
+  }
+}
