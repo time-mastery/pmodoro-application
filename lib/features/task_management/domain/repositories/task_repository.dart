cb65f71c8d75e0b8c9fc4be133e35b12178b5d2c
mahmoud <eslamimahmoud98@gmail.com>
2022-11-14 22:25:10 +0330

    Task repository added

---
+++ lib/features/task_management/domain/repositories/task_repository.dart
@@ -0,0 +1,8 @@
+import 'package:pomodore/core/resources/params.dart';
+
+abstract class TaskRepository {
+  Future addTask(TaskParams task);
+  Future getTaskByDate(DateTime date);
+  Future getTaskById(String id);
+  Future deleteTask(String id);
+}
