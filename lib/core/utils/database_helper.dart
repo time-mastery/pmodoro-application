946cfa81eec897d5fd9a07106e81584f94e23753
mahmoud <eslamimahmoud98@gmail.com>
2022-12-02 21:16:24 +0330

    sqflite database and database_helper.dart added

---
+++ lib/core/utils/database_helper.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+// ignore: depend_on_referenced_packages
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "pmodore.db";
+  static const _databaseVersion = 1;
+
+  static const taskTable = 'tasks';
+
+  static Database? _database;
+
+  static Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  static _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  static Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $taskTable (
+            _id INTEGER PRIMARY KEY,
+            uid TEXT NOT NULL,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            deadLineTime TEXT NOT NULL,
+            doneTime TEXT NOT NULL,
+            done INTEGER NOT NULL,
+          )
+          ''');
+  }
+}
--- lib/di.dart
+++ lib/di.dart
@@ -1,3 +1,19 @@
+import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pomodore/core/utils/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
 
+final getIt = GetIt.instance;
 
-void inject() {}
+Future inject() async {
+  await Hive.initFlutter();
+  Box appBox = await Hive.openBox('app_box');
+
+  getIt.registerLazySingleton<Box>(
+    () => appBox,
+    dispose: (param) => param.close(),
+  );
+
+  Database db = await DatabaseHelper.database;
+  getIt.registerSingleton<Database>(db);
+}
