deef94960c1bdf88d1b66071adb079ce1541d7d2
mahmoud <eslamimahmoud98@gmail.com>
2022-12-27 21:07:05 +0330

    Pomodoro table creation method added

--- lib/core/utils/database_helper.dart
+++ lib/core/utils/database_helper.dart
@@ -11,6 +11,7 @@ class DatabaseHelper {
 
   static const taskTable = 'tasks';
   static const categoryTable = 'categories';
+  static const pomodoroTable = 'pomodoros';
 
   static Database? _database;
 
@@ -48,6 +49,13 @@ class DatabaseHelper {
           CREATE TABLE $categoryTable (
             _id INTEGER PRIMARY KEY,
             title TEXT NOT NULL
+            )
+          ''');
+    batch.execute('''
+          CREATE TABLE $pomodoroTable (
+            _id INTEGER PRIMARY KEY,
+            taskUid INTEGER NOT NULL
+            secDuration TEXT NOT NULL
             )
           ''');
     await batch.commit();
