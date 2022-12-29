11024f069194b1b678a4fc5fbdce333df3a0d3c0
mahmoud <eslamimahmoud98@gmail.com>
2022-12-29 14:10:40 +0330

    Pomodoro entity and model added

---
+++ lib/features/task_management/data/models/pomodoro_model.dart
@@ -0,0 +1,15 @@
+import 'package:pomodore/features/task_management/domain/entities/pomodoro_entity.dart';
+
+class PomodoroModel extends PomodoroEntity {
+  const PomodoroModel(int id, int duration, String? taskUid, String datetime)
+      : super(id: id, duration: duration, taskUid: taskUid, dateTime: datetime);
+
+  static fromQueryToPomodoroModel(Map<String, dynamic> item) => PomodoroModel(
+      item["_id"], item["duration"], item["taskUid"], item["datetime"]);
+
+  static toDbQuery(PomodoroEntity item) => {
+        "taskUid": item.taskUid,
+        "duration": item.duration,
+        "dateTime": item.dateTime,
+      };
+}
---
+++ lib/features/task_management/domain/entities/pomodoro_entity.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class PomodoroEntity extends Equatable {
+  final int id, duration;
+  final String? taskUid;
+  final String dateTime;
+
+  const PomodoroEntity({
+    required this.id,
+    required this.duration,
+    required this.dateTime,
+    this.taskUid,
+  });
+
+  @override
+  List<Object?> get props => [id, duration, taskUid];
+}
