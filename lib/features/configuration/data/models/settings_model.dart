176fee2bb620a0b89aa1b674bad274f428d4fbcc
mahmoud <eslamimahmoud98@gmail.com>
2023-01-04 04:54:08 +0330

    refactor (settings feature) : add settings entity and settings model

---
+++ lib/features/configuration/data/models/settings_model.dart
@@ -0,0 +1,32 @@
+import 'package:pomodore/features/configuration/domain/entities/settings_entity.dart';
+
+class SettingsModel extends SettingsEntity {
+  const SettingsModel(bool notification, bool sound, bool vibration,
+      bool appUpdates, bool newTips, bool ads)
+      : super(
+          notification: notification,
+          sound: sound,
+          vibration: vibration,
+          ads: ads,
+          appUpdates: appUpdates,
+          newTips: newTips,
+        );
+
+  static toDbQuery(SettingsEntity model) => {
+        "notification": model.notification,
+        "sound": model.sound,
+        "vibration": model.vibration,
+        "ads": model.ads,
+        "appUpdates": model.appUpdates,
+        "newTips": model.newTips,
+      };
+
+  static fromQueryToSettingsModel(Map<String, dynamic> item) => SettingsEntity(
+        notification: item["notification"],
+        sound: item["sound"],
+        vibration: item["vibration"],
+        appUpdates: item["appUpdates"],
+        newTips: item["newTips"],
+        ads: item["ads"],
+      );
+}
