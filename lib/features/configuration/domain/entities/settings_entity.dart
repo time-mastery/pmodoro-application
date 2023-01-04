5f0dc14238897562969e39b35797c3ef4a3968b8
mahmoud <eslamimahmoud98@gmail.com>
2023-01-04 04:54:08 +0330

    refactor (settings feature) : add settings entity and settings model

---
+++ lib/features/configuration/domain/entities/settings_entity.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class SettingsEntity extends Equatable {
+  final bool notification;
+  final bool sound;
+  final bool vibration;
+  final bool appUpdates;
+  final bool newTips;
+  final bool ads;
+
+  const SettingsEntity(
+      {required this.notification,
+      required this.sound,
+      required this.vibration,
+      required this.appUpdates,
+      required this.newTips,
+      required this.ads});
+
+  @override
+  List<Object?> get props =>
+      [notification, sound, vibration, appUpdates, newTips, ads];
+}
