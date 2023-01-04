fc2c8a7725f23dc6499cb4210a6d32d6bb115f5f
mahmoud <eslamimahmoud98@gmail.com>
2023-01-04 04:39:32 +0330

    (refactor)(storage.dart): implement required storage methods

--- lib/core/utils/storage.dart
+++ lib/core/utils/storage.dart
@@ -1,9 +1,24 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class Storage {
+class FStorage {
+  static const notificationKey = "_notification_";
+  static const soundKey = "_sound_";
+  static const vibrationKey = "_vibration_";
+  static const appUpdateKey = "_update_";
+  static const newTipKey = "_tip_";
+  static const showAdsKey = "_ads_";
+
   static late FlutterSecureStorage flutterSecureStorage;
 
   static initialize() {
-    flutterSecureStorage = FlutterSecureStorage();
+    flutterSecureStorage = const FlutterSecureStorage();
   }
+
+  static write(String key, dynamic value) async =>
+      await flutterSecureStorage.write(key: key, value: value);
+
+  static delete(String key) async =>
+      await flutterSecureStorage.delete(key: key);
+
+  static read(String key) async => await flutterSecureStorage.read(key: key);
 }
