f206e4d3d0bad5dfd72b3e24d272d6257ead1710
mahmoud <eslamimahmoud98@gmail.com>
2023-02-14 04:35:46 +0330

    feat: add new method to repository

--- lib/features/configuration/data/repositories/settings_repository_impl.dart
+++ lib/features/configuration/data/repositories/settings_repository_impl.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/src/material/theme_data.dart';
 import 'package:pomodore/core/resources/params/settings_params.dart';
 import 'package:pomodore/features/configuration/data/data_sources/settings_local_data_source.dart';
+import 'package:pomodore/features/configuration/data/models/theme_model.dart';
 import 'package:pomodore/features/configuration/domain/entities/settings_entity.dart';
+import 'package:pomodore/features/configuration/domain/entities/theme_entity.dart';
 import 'package:pomodore/features/configuration/domain/repositories/settings_repository.dart';
 
 class SettingsRepositoryImpl implements SettingsRepository {
@@ -59,6 +62,34 @@ class SettingsRepositoryImpl implements SettingsRepository {
     try {
       String locale = await localDataSources.getLocale();
       result = Right(locale);
+    } catch (e) {
+      result = const Left("error");
+    }
+
+    return result;
+  }
+
+  @override
+  Future<Either<String, ThemeData>> changeTheme(ThemeEntity theme) async {
+    late Either<String, ThemeData> result;
+
+    try {
+      await localDataSources.changeTheme(theme.title);
+      result = Right(theme.theme);
+    } catch (e) {
+      result = const Left("error");
+    }
+
+    return result;
+  }
+
+  @override
+  Future<Either<String, ThemeData>> getTheme() async {
+    late Either<String, ThemeData> result;
+
+    try {
+      var themeName = await localDataSources.getTheme();
+      result = Right(ThemeModel.titleToTheme(themeName));
     } catch (e) {
       result = const Left("error");
     }
--- lib/features/configuration/domain/repositories/settings_repository.dart
+++ lib/features/configuration/domain/repositories/settings_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:pomodore/core/resources/params/settings_params.dart';
 import 'package:pomodore/features/configuration/domain/entities/settings_entity.dart';
+import 'package:pomodore/features/configuration/domain/entities/theme_entity.dart';
 
 abstract class SettingsRepository {
   Future<Either<String, SettingsEntity>> getSettings();
@@ -11,4 +13,8 @@ abstract class SettingsRepository {
   Future<Either<String, String>> changeLocale(String langCode);
 
   Future<Either<String, String>> getLocale();
+
+  Future<Either<String, ThemeData>> getTheme();
+
+  Future<Either<String, ThemeData>?> changeTheme(ThemeEntity theme);
 }
