8b81d6e9ff32975b573a75464c8834b7a0ae5a80
mahmoud <eslamimahmoud98@gmail.com>
2023-03-09 01:02:49 +0330

    test: fix timer bloc test

--- test/features/task_management/presentation/blocs/timer_bloc/timer_bloc_test.dart
+++ test/features/task_management/presentation/blocs/timer_bloc/timer_bloc_test.dart
@@ -1,24 +1,44 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:pomodore/core/utils/ticker.dart';
+import 'package:pomodore/features/task_management/domain/usecases/add_pomodoro_to_db_usecase.dart';
+import 'package:pomodore/features/task_management/domain/usecases/restore_timer_state_usecase.dart';
+import 'package:pomodore/features/task_management/domain/usecases/save_timer_state_usecase.dart';
+import 'package:pomodore/features/task_management/presentation/blocs/timer_bloc/timer_bloc.dart';
 
+import 'timer_bloc_test.mocks.dart';
+
+@GenerateMocks([
+  AddPomodoroToDbUseCase,
+  RestoreTimerStateUseCase,
+  SaveTimerStateUseCase,
+])
 void main() {
-  // todo : check why blocTest not work
-  // group("Timer Bloc", () {
-  //   late TimerBloc timerBloc;
-  //   int duration = 60 * 25;
-  //
-  //   setUp(() => timerBloc = TimerBloc(ticker: const Ticker()));
-  //
-  //   blocTest(
-  //     "emit [TimerInProcess] when start timer",
-  //     build: () => timerBloc,
-  //     act: (bloc) => bloc.add(TimerStarted(duration)),
-  //     expect: () => [TimerInProgress(duration)],
-  //   );
-  //
-  //   blocTest(
-  //     "emit [TimerInitial] when reset timer",
-  //     build: () => timerBloc,
-  //     act: (bloc) => bloc.add(TimerReset()),
-  //     expect: () => [TimerInitial(duration)],
-  //   );
-  // });
+  group("Timer Bloc", () {
+    late TimerBloc timerBloc;
+    int duration = 60 * 25;
+
+    setUp(() => timerBloc = TimerBloc(
+          ticker: const Ticker(),
+          addPomodoroToDbUseCase: MockAddPomodoroToDbUseCase(),
+          restoreTimerStateUseCase: MockRestoreTimerStateUseCase(),
+          saveTimerStateUseCase: MockSaveTimerStateUseCase(),
+        ));
+
+    blocTest(
+      "emit [TimerInProcess] when start timer",
+      build: () => timerBloc,
+      act: (bloc) => bloc.add(TimerStarted(duration)),
+      expect: () =>
+          [StartTimerLoading(duration), StartTimerWithoutTaskFailure(duration)],
+    );
+
+    blocTest(
+      "emit [TimerInitial] when reset timer",
+      build: () => timerBloc,
+      act: (bloc) => bloc.add(TimerReset()),
+      expect: () => [TimerInitial(duration)],
+    );
+  });
 }
